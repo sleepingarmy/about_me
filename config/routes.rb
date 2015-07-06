91128ac717e9e001f74d3888e7a242c3c92eda0f
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-06 10:56:59 -0600

    created about and contact controllers with index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact/index'
+
+  get 'about/index'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
