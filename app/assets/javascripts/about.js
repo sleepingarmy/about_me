e9253d018f1761b06ea31e809c56828ffc0b7285
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-10 11:41:53 -0600

    first heroku commit

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+  $("#interests").click(function() {
+    $("#interests").next().toggle();
+  });
   
+  $("#education").click(function() {
+    $("#education").next().toggle();
+  });
+
+  $("#work").click(function() {
+    $("#work").next().toggle();
+  });
+
+  $("#goals").click(function() {
+    $("#goals").next().toggle();
+  });
 })
